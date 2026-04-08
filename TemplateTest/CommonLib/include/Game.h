045711a22d6cf9f712dda77f4f5f4d511efc47a9
Ignaciotema7b <ignacio.tejera@7bridges.bet>
2026-04-07 21:38:24 -0300

    Update Game.h

--- TemplateTest/CommonLib/include/Game.h
+++ TemplateTest/CommonLib/include/Game.h
@@ -7,6 +7,21 @@
 using namespace std;
 
 
+#ifdef CI  // Definida en CI
+#include <cstring>  // para strcmp
+
+inline int pause_override(const char* cmd) {
+    if (cmd && strcmp(cmd, "pause") == 0) {
+        return 0;  // Ignora el pause
+    }
+    return std::system(cmd); // Llama al system normal para otros comandos
+}
+
+#define system(cmd) pause_override(cmd)
+
+#endif
+
+
  /**
   * @class Game
   * \~english
