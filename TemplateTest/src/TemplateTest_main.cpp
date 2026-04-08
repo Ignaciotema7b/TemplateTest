316921680bc6d56ef0df6ed4cc2eadd5b59a4777
Ignaciotema7b <ignacio.tejera@7bridges.bet>
2026-04-07 21:43:28 -0300

    Update TemplateTest_main.cpp

--- TemplateTest/src/TemplateTest_main.cpp
+++ TemplateTest/src/TemplateTest_main.cpp
@@ -23,6 +23,15 @@ using namespace std;
 
 #include "TemplateTest.h"
 
+#ifdef CI
+inline int system(const char* cmd) {
+    if (cmd && std::strcmp(cmd, "pause") == 0) {
+        return 0; // Ignora solo pause
+    }
+    return std::system(cmd); // Otros comandos siguen funcionando
+}
+#endif
+
 
 int main()
 {
